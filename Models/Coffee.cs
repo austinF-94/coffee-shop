7d4e09fce04ec99a652fb0bc64fabfa66867b96f
A-Fog <A-Fogle1994@outlook.com>
2023-12-01 13:38:16 -0500

    Add project files.

---
+++ Models/Coffee.cs
@@ -0,0 +1,35 @@
+namespace coffee_shop.Models;
+
+public class Coffee {
+
+
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+
+
+    public string getName()
+    {
+        return Name;
+    }
+    public void setName(string name) 
+    {
+        this.Name = name;
+    }
+    public string getName()
+    {
+        return Name;
+    }
+    public void setName(string name) 
+    {
+        this.Name = name;
+    }
+    public string getName()
+    {
+        return Name;
+    }
+    public void setName(string name) 
+    {
+        this.Name = name;
+    }
+}
